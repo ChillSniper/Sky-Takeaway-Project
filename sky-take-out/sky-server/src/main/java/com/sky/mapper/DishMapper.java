a5a7102d953956174220504e74546c545275ae53
HerbertLu <3242473480@qq.com>
2025-10-27 17:18:46 +0800

    Day12 update ! ! ! 完结撒花！！！ 下一个项目：黑马点评（Redis）启动！

--- sky-take-out/sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-take-out/sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -11,6 +11,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface DishMapper {
@@ -80,4 +81,12 @@ public interface DishMapper {
      */
     @Select("select * from dish where id in (select dish_id from setmeal_dish where setmeal_id = #{id})")
     List<Dish> getBySetmealId(Long id);
+
+    /**
+     * 根据条件统计菜品数量
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
+
 }
